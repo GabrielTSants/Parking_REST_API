c85aaabdbd1d43bc4847eefa267283aa3754b65d
GabrielTSants <rlgts@hotmail.com>
2021-11-02 23:13:02 -0300

    Correcao de dump do CRUD

--- parking.sql
+++ parking.sql
@@ -1,32 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.6.4-MariaDB, for Linux (x86_64)
 --
--- Host: localhost
--- Tempo de geração: 24/07/2021 às 20:48
--- Versão do servidor: 10.6.3-MariaDB
--- Versão do PHP: 8.0.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: parking_management
+-- ------------------------------------------------------
+-- Server version	10.6.4-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `parking_management`
+-- Table structure for table `parking`
 --
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `parking`
---
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE IF NOT EXISTS `parking` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `customer` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -37,19 +30,25 @@ CREATE TABLE IF NOT EXISTS `parking` (
   `obs` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `parking`
+-- Dumping data for table `parking`
 --
 
-INSERT IGNORE INTO `parking` (`id`, `customer`, `car`, `date`, `dateCreated`, `status`, `obs`) VALUES
-(1, 'Bob', 'Honda Civic', '2021-06-11 00:06:00', '2021-07-24 17:07:19', 'Parked', 'Not paid yet'),
-(2, 'Lucas', 'Chevrolet Kadett', '2021-07-25 00:07:00', '2021-07-24 17:07:26', 'Paid', 'Paid on cash'),
-(3, 'Vanessa', 'Volkswagen Beetle', '2021-07-29 00:07:00', '2021-07-24 17:07:54', 'Paid', 'Paid on cash'),
-(4, 'Larissa', 'Ford Focus', '2021-06-02 00:06:00', '2021-07-24 17:07:49', 'Paid', 'Rental customer'),
-(5, 'Alex', 'Ford Escort', '2021-06-11 00:06:00', '2021-07-24 17:07:54', 'Parked', 'Rental customer');
-COMMIT;
+LOCK TABLES `parking` WRITE;
+/*!40000 ALTER TABLE `parking` DISABLE KEYS */;
+INSERT INTO `parking` VALUES (1,'Bob','Honda Civic','2021-06-11 00:06:00','2021-07-24 17:07:19','Parked','Not paid yet'),(2,'Lucas','Chevrolet Kadett','2021-07-25 00:07:00','2021-07-24 17:07:26','Paid','Paid on cash'),(3,'Vanessa','Volkswagen Beetle','2021-07-29 00:07:00','2021-07-24 17:07:54','Paid','Paid on cash'),(4,'Larissa','Ford Focus','2021-06-02 00:06:00','2021-07-24 17:07:49','Paid','Rental customer'),(5,'Alex','Ford Escort','2021-06-11 00:06:00','2021-07-24 17:07:54','Parked','Rental customer');
+/*!40000 ALTER TABLE `parking` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-02 23:09:01
